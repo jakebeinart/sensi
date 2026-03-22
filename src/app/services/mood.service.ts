fc58a9aeab8e55da0d434ecf9efb3facfb4f95ef
Jake Beinart <jake.beinart@gmail.com>
2026-03-22 10:12:13 -0500

    update color palette

--- src/app/services/mood.service.ts
+++ src/app/services/mood.service.ts
@@ -26,7 +26,7 @@ export class MoodService {
     return total / entries.length;
   });
 
-  getColorClass(rating: number | null | undefined) {
+  getColorClass2(rating: number | null | undefined) {
     switch (rating) {
       case 1:
         return 'bg-red-600';
@@ -42,6 +42,33 @@ export class MoodService {
         return 'bg-yellow-300';
       case 7:
         return 'bg-green-300';
+      case 8:
+        return 'bg-green-400';
+      case 9:
+        return 'bg-green-500';
+      case 10:
+        return 'bg-green-600';
+      default:
+        return '';
+    }
+  }
+
+  getColorClass(rating: number | null | undefined) {
+    switch (rating) {
+      case 1:
+        return 'bg-red-700';
+      case 2:
+        return 'bg-red-600';
+      case 3:
+        return 'bg-orange-600';
+      case 4:
+        return 'bg-amber-600';
+      case 5:
+        return 'bg-yellow-600';
+      case 6:
+        return 'bg-yellow-500';
+      case 7:
+        return 'bg-seven';
       case 8:
         return 'bg-green-400';
       case 9:
